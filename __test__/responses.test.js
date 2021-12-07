bff6587dc90aa74f5425c9225d12eba6585e54d1
Fedorov Ivan <sirarthurjohnevans@live.com>
2021-12-07 12:38:46 +0300

    change order of tests

--- __test__/responses.test.js
+++ __test__/responses.test.js
@@ -3,19 +3,23 @@ import getFilteringStr from '../index';
 
 const request = supertest('https://jsonplaceholder.typicode.com');
 const id1 = 7;
-const id2 = 11;
+const id2 = 2;
 const title = 'voluptatem laborum magni';
 
-describe('Smoke test', () => {
-  it('filtering existing posts', async () => {
+describe('Smoke test of filtering request', () => {
+  it('has status OK', async () => {
+    const response1 = await request.get(getFilteringStr(id1, title));
+    const response2 = await request.get(getFilteringStr(id2, title));
+    expect(response1.status).toBe(200);
+    expect(response2.status).toBe(200);
+  });
+  it('has not empty body', async () => {
     const response = await request.get(getFilteringStr(id1, title));
-    expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body).toHaveLength(1);
   });
-  it('filtering non-existent posts', async () => {
+  it('has empty body', async () => {
     const response = await request.get(getFilteringStr(id2, title));
-    expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body).toHaveLength(0);
   });
