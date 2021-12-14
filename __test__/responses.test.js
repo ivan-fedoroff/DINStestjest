120327069b61a4a854950551673f8330900d2683
Fedorov Ivan <sirarthurjohnevans@live.com>
2021-12-14 15:08:39 +0300

    add jest-in-case for parameterization test; change tests

--- __test__/responses.test.js
+++ __test__/responses.test.js
@@ -1,26 +1,30 @@
 import supertest from 'supertest';
+import cases from 'jest-in-case';
 import getFilteringStr from '../index';
 
 const request = supertest('https://jsonplaceholder.typicode.com');
-const id1 = 7;
-const id2 = 2;
 const title = 'voluptatem laborum magni';
 
 describe('Smoke test of filtering request', () => {
-  it('has status OK', async () => {
-    const response1 = await request.get(getFilteringStr(id1, title));
-    const response2 = await request.get(getFilteringStr(id2, title));
-    expect(response1.status).toBe(200);
-    expect(response2.status).toBe(200);
+  cases('have status OK', async (opts) => {
+    const response = await request.get(opts.input);
+    expect(response.status).toBe(opts.result); // eslint-disable-line
+  }, {
+    'test with not empty result': { input: getFilteringStr(7, title), result: 200 },
+    'test with empty result': { input: getFilteringStr(2, title), result: 200 },
   });
-  it('has not empty body', async () => {
-    const response = await request.get(getFilteringStr(id1, title));
-    expect(Array.isArray(response.body)).toBeTruthy();
-    expect(response.body).toHaveLength(1);
+  cases('have array in body', async (opts) => {
+    const response = await request.get(opts.input);
+    expect(Array.isArray(response.body)).toBeTruthy(); // eslint-disable-line
+  }, {
+    'test with not empty result': { input: getFilteringStr(7, title) },
+    'test with empty result': { input: getFilteringStr(2, title) },
   });
-  it('has empty body', async () => {
-    const response = await request.get(getFilteringStr(id2, title));
-    expect(Array.isArray(response.body)).toBeTruthy();
-    expect(response.body).toHaveLength(0);
+  cases('have different length of body', async (opts) => {
+    const response = await request.get(opts.input);
+    expect(Array.isArray(response.body)).toHaveLength(opts.result); // eslint-disable-line
+  }, {
+    'test with not empty result: length is 1': { input: getFilteringStr(7, title), result: 1 },
+    'test with empty result: length is 0': { input: getFilteringStr(2, title), result: 0 },
   });
 });
